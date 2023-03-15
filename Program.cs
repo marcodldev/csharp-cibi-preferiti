69d0c381f5401c4bd46d5dcf56d7e0301b781465
Emir-Software <125670961+Emir-Software@users.noreply.github.com>
2023-03-15 16:49:53 +0100

    + primi 3 punti

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+string[] cibipreferiti = { "Lasagne", "Pollo", "Cheesecake", "PatateFritte", "Sushi" };
+
+
+
+Console.WriteLine("La lista contiene" + " " + cibipreferiti.Length + " " + "cibi");
+
+
+for (int i = 0; i < cibipreferiti.Length; i++)
+{
+    Console.WriteLine(cibipreferiti[i] + " " + (i+1) );
+}
+
+
+Console.WriteLine("Al primo posto abbiamo:" + " " + cibipreferiti[0]);
+
